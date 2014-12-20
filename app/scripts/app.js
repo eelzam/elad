9e662a2bc81f9d087b3cbf23a7f8b22f97f9a80e
Elad Elzam <elzam12000@yahoo.com>
2014-12-20 09:20:07 +0200

    angular by elzam

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularGeneratedApp
+ * @description
+ * # angularGeneratedApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularGeneratedApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularGeneratedApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularGeneratedApp
+ */
+angular.module('angularGeneratedApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
